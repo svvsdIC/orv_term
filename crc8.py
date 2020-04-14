35f6f8aa97ee72ae5f56b765ef13e7545f3542de
pablo maurin <maurin@gmail.com>
2020-04-14 12:37:50 -0600

    Add TODOs

--- crc8.py
+++ crc8.py
@@ -17,6 +17,12 @@ CRC_TABLE = bytes( (
     0x74, 0x2a, 0xc8, 0x96, 0x15, 0x4b, 0xa9, 0xf7, 0xb6, 0xe8, 0x0a, 0x54, 0xd7, 0x89, 0x6b, 0x35,
 ) )
 
+# TODO: turn into a class, with init, continuation, and finalize
+# should have static method for computing CRC
+# export single method at module level
+#
+# ensure buf is array of bytes
+
 def crc8(buf):
     crc = 0x00
 
