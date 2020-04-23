8138c544d414426f7afe7043166c1594098f1d39
pablo maurin <maurin@gmail.com>
2020-04-22 19:29:03 -0600

    Clean up code, prep for TK application object
    Prep basic TK application to start laying out components

--- orvt.py
+++ orvt.py
@@ -5,45 +5,6 @@ import queue
 import serial
 import tkinter as tk
 import tkinter.ttk as ttk
-from enum import Enum
-from threading import Thread
-from types import SimpleNamespace
-
-class Messages(Enum):
-    WRITEMSG = 1
-
-# controller
-def workcycle(guiref, model, queue):
-    """
-    Fun worker thread
-
-    :param guiref:
-    :param model:
-    :param queue:
-    :return:
-    """
-    while True:
-        msg = queue.get()
-        '''Manage the message extracted from the queue'''
-        print(msg)
-        '''Take care of other tasks, like refreshing the UI with newly received content'''
-
-# view
-def gui(root,q):
-    """
-    Builds the GUI for ORV Term
-
-    :param root:
-    :return:
-    """
-
-    '''Build a Tkinter GUI'''
-    root.title('orvt')
-    ttk.Label(root, text='Hello World').pack()
-    '''Return a collection of significant widget references'''
-
-    return None
-
 
 class Orvt(tk.Tk):
     """
@@ -53,10 +14,18 @@ class Orvt(tk.Tk):
         super().__init__()
 
         self.title('orvt')
-        ttk.Label(self, text='Hello World').pack()
+        self.resizable(True, True)
 
+        # Layout UI objects
+        # self.main_frame = tk.Frame(self, width=500, height=300, bg="lightgrey")
 
+        self.serial_text = tk.Text(self)
+        self.serial_text.pack(expand=1, fill=tk.BOTH)
+        self.serial_text.pack()
+        self.serial_text.config(state=tk.DISABLED)
 
+        self.cmd_entry = ttk.Entry(self)
+        self.cmd_entry.pack()
 
 
 def main(serial_port, baudrate):
@@ -67,21 +36,17 @@ def main(serial_port, baudrate):
     q = queue.Queue()
 
     # Create GUI elements
-    guiref = gui(root, q)
-
-    # Initialize model application will be working on
-    model = SimpleNamespace(port=port)
     orvt = Orvt()
 
-    t = Thread(target=workcycle, args=(guiref, model, q,))
-    #t.daemon = True  # daemonize it, so it will die when the program stops
-    t.start()
-
-    # Start the Application main loop
-    #root.protocol("WM_DELETE_WINDOW", callback)
+    # Begin running the UI.
+    # This function will not return until the Tk application is closed
     orvt.mainloop()
 
+    #clean up
     print('mainloop exited')
+
+    if port.isOpen():
+        port.close()
 
     # TODO: send thread termination messages and join
     # wait for a bit, then forcefully end threads
