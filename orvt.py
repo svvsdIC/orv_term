b76a733b4e6b6340a32347fc75afa650caac9175
pablo maurin <maurin@gmail.com>
2020-04-22 20:01:40 -0600

    Rename communication log widget

--- orvt.py
+++ orvt.py
@@ -19,10 +19,10 @@ class Orvt(tk.Tk):
         # Layout UI objects
         # self.main_frame = tk.Frame(self, width=500, height=300, bg="lightgrey")
 
-        self.serial_text = tk.Text(self)
-        self.serial_text.pack(expand=1, fill=tk.BOTH)
-        self.serial_text.pack()
-        self.serial_text.config(state=tk.DISABLED)
+        self.comms_log = tk.Text(self, wrap=tk.NONE)
+        self.comms_log.pack(expand=1, fill=tk.BOTH)
+        self.comms_log.pack()
+        self.comms_log.config(state=tk.DISABLED)
 
         self.cmd_entry = ttk.Entry(self)
         self.cmd_entry.pack()
