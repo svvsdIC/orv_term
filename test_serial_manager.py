efb78c7e06bedcd12e6e83a816830c4d0c85e334
pablo maurin <maurin@gmail.com>
2020-04-30 19:12:10 -0600

    Don't do '*' imports

--- test_serial_manager.py
+++ test_serial_manager.py
@@ -1,10 +1,8 @@
 #import unittest
-from serial_manager import *
+import serial_manager as sm
 
 
 if __name__ == '__main__':
-    with open_port('loop://', 115200) as (rx, tx):
+    with sm.open_port('loop://', 115200) as (rx, tx):
         print(rx)
         print(tx)
-        # time.sleep(10)
-        # stop_rxtx_loops()
